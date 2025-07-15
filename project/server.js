2aebde11c35524dc4e752925327ad204a7fa2d8a
chino4242 <ryanj.contino@gmail.com>
2025-07-15 15:19:03 -0400

    completed stage02 add getCustomer

--- project/server.js
+++ project/server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 4000;
+const da = require("./data-access");
 
 app.use(express.static('public'));
+
+app.get("/customers", async(req, res) => {
+    const cust = await da.getCustomers();
+    res.send(cust);
+});
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`)
