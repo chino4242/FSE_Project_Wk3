8ae440f91ce53d1800a09f79dc8706164a1a096e
chino4242 <ryanj.contino@gmail.com>
2025-07-16 15:52:49 -0400

    finalizes functionality for first 8 stages

--- project/server.js
+++ project/server.js
@@ -30,29 +30,27 @@ app.get('/customers/:id', async (req, res) => {
 
 app.post('/customers', async(req, res) => {
     const newCustomer = req.body;
-    if (newCustomer === null || req.body != {}) {
-        res.status(400);
-        res.send("missing request body");
+    if (!newCustomer || newCustomer === null || Object.keys(newCustomer).length===0) {
+        res.status(400).send("missing request body");
+    } 
+    const [status, id, errMessage] = await da.addCustomer(newCustomer);
+    if (status ==='success') {
+        res.status(201);
+        let response = {...newCustomer};
+        response["_id"] = id;
+        res.send(response);
     } else {
-        const [status, id, errMessage] = await da.addCustomer(newCustomer);
-        if (status ==='success') {
-            res.status(201);
-            let response = {...newCustomer};
-            response["_id"] = id;
-            res.send(response);
-        } else {
-            res.status(400);
-            res.send(errMessage);
-        }
+        res.status(400);
+        res.send(errMessage);
     }
-});
+}
+);
 
 app.put('/customers/:id', async (req, res) => {
     const id = req.params.id;
     const updatedCustomer = req.body;
-    if (updatedCustomer === null || req.body != {}) {
-        res.status(404);
-        res.send("missing request body");
+    if (updatedCustomer === null || Object.keys(newCustomer).length===0) {
+        res.status(404).send('missing request body');
     } else {
         delete updatedCustomer._id;
         const [message, errMessage] = await da.updateCustomer(updatedCustomer);
@@ -76,7 +74,7 @@ app.delete('/customers/:id', async (req, res) => {
     }
 })
 
-app.get("/reset", async(req, res) => {
+app.post("/reset", async(req, res) => {
     const [result, error] = await da.resetCustomers();
     if (error) {
         return res.status(500).json({error: 'Failed to reset'});
